05de3e5bb35376dcef35435cf470e0483020fbc2
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2023-01-07 13:17:49 +0530

    Added: AddContact Function and Retrieve Function

--- addressBook.go
+++ addressBook.go
@@ -10,6 +10,17 @@ import (
 
 var db *sql.DB
 
+type Contact struct {
+	id          int
+	FirstName   string
+	LastName    string
+	Address     string
+	City        string
+	State       string
+	PhoneNumber string
+	Email       string
+}
+
 func main() {
 	var err error
 	//"UserName:Password@tcp(portNumber)/databaseName"
@@ -24,5 +35,83 @@ func main() {
 	}
 	fmt.Println("Connected")
 	fmt.Println("**************** Wel-Come To Address Book ****************")
+	Operation()
+}
+
+func Operation() {
+	var option int
+	fmt.Println("\n-------------- Select which operation do you wanna perform --------------\n1.Add Contact\n2.Display Contacts\n3.Exit")
+	fmt.Scanln(&option)
+	switch option {
+	case 1:
+		addContact()
+		//Operation()
+	case 2:
+		fmt.Println(DatabaseReader())
+		//Operation()
+	case 3:
+		//break
+		return
+	}
+	Operation()
+}
+
+func DatabaseReader() ([]Contact, error) {
+	var contacts []Contact
+
+	rows, err := db.Query("SELECT * FROM Contact;")
+	if err != nil {
+		return nil, fmt.Errorf("error in query: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var person Contact
+
+		if err := rows.Scan(&person.id, &person.FirstName, &person.LastName, &person.Address, &person.City, &person.State, &person.PhoneNumber, &person.Email); err != nil {
+			return nil,
+				fmt.Errorf("error in query : %v", err)
+		}
+
+		contacts = append(contacts, person)
+
+	}
+	return contacts, nil
+}
+
+func addContact() (int64, error) {
+	var person Contact
+
+	fmt.Println("Enter First Name: ")
+	fmt.Scanln(&person.FirstName)
+
+	fmt.Println("Enter Last Name: ")
+	fmt.Scanln(&person.LastName)
+
+	fmt.Println("Enter Address: ")
+	fmt.Scanln(&person.Address)
+
+	fmt.Println("Enter City: ")
+	fmt.Scanln(&person.City)
+
+	fmt.Println("Enter State: ")
+	fmt.Scanln(&person.State)
+
+	fmt.Println("Enter Phone Number: ")
+	fmt.Scanln(&person.PhoneNumber)
+
+	fmt.Println("Enter Email-Id: ")
+	fmt.Scanln(&person.Email)
+
+	result, err := db.Exec("INSERT INTO Contact(FirstName, LastName, Address, City, State, PhoneNumber, Email) VALUES (?, ?, ?, ?, ?, ?, ?)", person.FirstName, person.LastName, person.Address, person.City, person.State, person.PhoneNumber, person.Email)
+	if err != nil {
+		return 0, fmt.Errorf("add person: %v", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("add person: %v", err)
+	}
+	return id, nil
 
 }
